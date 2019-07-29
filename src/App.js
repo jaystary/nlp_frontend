24d25dc3157dd020e625f3227c66d2c9b1cf5c04
Jay <jstary80@gmail.com>
2019-07-29 21:21:38 +0200

    multiple parts loading

--- src/App.js
+++ src/App.js
@@ -11,71 +11,71 @@ class App extends Component {
       sentence: "",
       id: "",
       messages: [],
-      };
+    };
 
     this.messagewindowElement = React.createRef();
   }
-  
+
 
   handleChange(event) {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   }
 
-/*
-  sendMessage(message, id, route) {
-    console.log("sending")
-    socket.emit(
-      'send_message',
-      {
-        route,
-        id: id,
-        body: message,
-        timeStamp: Date.now()
-      }
-    )
-  }
-
-  setSocketListeners() {
-    socket.on("message", data => {
-      console.log("Logging incomcing Message")
-      console.log(data.message);
-    });
-
-    socket.on("message_sent", message => {
-      this.setState({ messages: [...this.state.messages, message] }, () => {
-        window.localStorage.setItem(
-          "messages",
-          JSON.stringify(this.state.messages)
-        );
-      });
-    });
-
-    socket.on("activate_socket", message => {
-      
-      console.log("connected");
-      //if (this.state.username) {
-      //  socket.emit('activate_user', { username: this.state.username })
-      //}
-    });
-
-    socket.on("send_message", message => {
-      
-      console.log("sending Message");
-      //if (this.state.username) {
-      //  socket.emit('activate_user', { username: this.state.username })
-      //}
-    });
-  }
-
-
- loadMessages() {
-    const savedMessages = window.localStorage.getItem("messages");
-    if (savedMessages) {
-      this.setState({ messages: JSON.parse(savedMessages) || [] });
+  /*
+    sendMessage(message, id, route) {
+      console.log("sending")
+      socket.emit(
+        'send_message',
+        {
+          route,
+          id: id,
+          body: message,
+          timeStamp: Date.now()
+        }
+      )
     }
-  }
-  */
+  
+    setSocketListeners() {
+      socket.on("message", data => {
+        console.log("Logging incomcing Message")
+        console.log(data.message);
+      });
+  
+      socket.on("message_sent", message => {
+        this.setState({ messages: [...this.state.messages, message] }, () => {
+          window.localStorage.setItem(
+            "messages",
+            JSON.stringify(this.state.messages)
+          );
+        });
+      });
+  
+      socket.on("activate_socket", message => {
+        
+        console.log("connected");
+        //if (this.state.username) {
+        //  socket.emit('activate_user', { username: this.state.username })
+        //}
+      });
+  
+      socket.on("send_message", message => {
+        
+        console.log("sending Message");
+        //if (this.state.username) {
+        //  socket.emit('activate_user', { username: this.state.username })
+        //}
+      });
+    }
+  
+  
+   loadMessages() {
+      const savedMessages = window.localStorage.getItem("messages");
+      if (savedMessages) {
+        this.setState({ messages: JSON.parse(savedMessages) || [] });
+      }
+    }
+    */
 
   getData = resp => {
     console.log(resp);
@@ -87,16 +87,16 @@ class App extends Component {
     console.log(resp)
   };
 
-  doSomething(){
+  doSomething() {
     socket.emit("getdata", "Hello World.");
   }
-  
+
 
   componentDidMount() {
     //this.loadMessages();
     //this.setSocketListeners();
     //this.messagewindowElement.current.submitMessage("Hello")
-   
+
     socket.on("getdata", this.getData);
     socket.on("echo", this.echoResponse);
     socket.emit("echo");
@@ -119,12 +119,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      < Header/>
+        < Header />
         <MessageWindow ref={this.messagewindowElement} />
         <form onSubmit={this.handleOnSubmit}>
-        <button type="submit">Submit for test</button>
-      </form>
-       Hello World
+          <button type="submit">Submit for test</button>
+        </form>
+        Hello Amy
       </div>
     );
   }
