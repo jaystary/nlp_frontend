5aa5c30e43b67c2d023ae9c6a192c9fbc7c239b4
Jay <jstary80@gmail.com>
2019-08-02 18:55:58 +0200

    Cleanup

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rotateReducer from "reducers/rotateReducer";
-
-function configureStore(state = { rotating: true }) {
-  return createStore(rotateReducer,state);
-}
-
-export default configureStore;
